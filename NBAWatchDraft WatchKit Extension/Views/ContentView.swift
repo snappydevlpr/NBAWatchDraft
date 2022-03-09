24754ada63649de1fbe65a1bbcef134f958127d0
Austin Suarez <austinmsuarez@gmail.com>
2022-03-08 21:46:12 -0600

    adding basic list to display draft

--- NBAWatchDraft WatchKit Extension/Views/ContentView.swift
+++ NBAWatchDraft WatchKit Extension/Views/ContentView.swift
@@ -10,8 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Content View")
+        List{
+            ForEach(0..<2){ _ in
+                //
+                NavigationLink(destination:EmptyView()){
+                    ZStack{
+                        Image("draft-menubackground")
+                        
+                        VStack(){
+                            Text("Round")
+                                .custom(font: .bold, size: 16)
+                            Text("1")
+                                .custom(font: .ultralight, size: 70)
+                        }.offset(x: -10, y: 10)
+                    }
+                }.listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            }
+        }.listStyle(CarouselListStyle())
+            .navigationBarTitle("NBA Draft")
     }
+    
+    var background:some View{
+        Rectangle().fill(Color("CardBackground"))
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
